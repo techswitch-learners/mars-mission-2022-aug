b8b5a218c1c48543025268c2bb5899dc4538ec59
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-09-06 14:11:41 +0100

    MM-07: Adding navbar html to Navbar.tsx

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -3,11 +3,20 @@ import { Link } from "react-router-dom";
 
 export const Navbar: React.FunctionComponent = () => {
   return <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li>Explore Mars</li>
-      <li>Fun Facts</li>
-      <li>Test Your Knowledge</li>
-    </ul>
+
+    <div id="menuToggle">
+      <input type="checkbox" />
+      
+      <span></span>
+      <span></span>
+      <span></span>
+      
+      <ul id="menu">
+        <li><Link to="/">Home</Link></li>
+        <li>Explore Mars</li>
+        <li>Fun Facts</li>
+        <li>Test Your Knowledge</li>
+      </ul>
+    </div>
   </nav>;
 };
