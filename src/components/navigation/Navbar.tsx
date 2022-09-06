f5a3972c8b619fa2d4d45420e1bdff22fc64532e
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-09-06 14:07:53 +0100

    MM-07: Adding dead links to navbar

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -5,6 +5,9 @@ export const Navbar: React.FunctionComponent = () => {
   return <nav>
     <ul>
       <li><Link to="/">Home</Link></li>
+      <li>Explore Mars</li>
+      <li>Fun Facts</li>
+      <li>Test Your Knowledge</li>
     </ul>
   </nav>;
 };
