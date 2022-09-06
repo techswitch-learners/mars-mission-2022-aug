abfc5820ad36d973eeb29eedf4ecf3851f4705cb
Konstantinos <kon.xen@:gmail.com>
2022-09-06 12:51:13 +0100

    empty Quiz component and empty SCSS

---
+++ src/components/quiz/Quiz.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Quiz: React.FunctionComponent = () => {
+    return (
+        <div>
+            <h1>This is the Quiz Component</h1>
+        </div>
+    );
+}
