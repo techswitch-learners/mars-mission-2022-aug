a70cafa0557fb7ba9eac3559875fd786adacf263
tomscambler <tomscambler@gmail.com>
2022-09-06 15:25:16 +0100

    MM-07: Moved hamburger up and to the left

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -8,16 +8,16 @@ export const Navbar: React.FunctionComponent = () => {
     <div id="menuToggle">
       <input type="checkbox" />
       
-      <span></span>
-      <span></span>
-      <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       
-      <ul id="menu">
-        <li><Link to="/">Home</Link></li>
-        <li>Explore Mars</li>
-        <li>Fun Facts</li>
-        <li>Test Your Knowledge</li>
-      </ul>
+        <ul id="menu">
+          <li><Link to="/">Home</Link></li>
+          <li>Explore Mars</li>
+          <li>Fun Facts</li>
+          <li>Test Your Knowledge</li>
+        </ul>
     </div>
   </nav>;
 };
