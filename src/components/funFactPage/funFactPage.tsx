c2474c36ae71e0f0c476f223c447ba2b4b0b8ace
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-09-06 12:20:58 +0100

    MM-11: Adding and exporting basic funfactpage function

--- src/components/funFactPage/funFactPage.tsx
+++ src/components/funFactPage/funFactPage.tsx
@@ -1,1 +1,10 @@
 import React from "react";
+
+export const FunFactPage: React.FunctionComponent = () => {
+    return (
+      <main>
+        <h1>FunFactPage</h1>
+        <p>FunFactPage</p>
+      </main>
+    );
+  };
