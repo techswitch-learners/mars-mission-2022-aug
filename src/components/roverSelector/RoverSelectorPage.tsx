95bac91f770a19fcd050b84f8e09af2cfb0a8976
Jack Hughes <Jack.Hughes@softwire.com>
2022-09-07 14:28:02 +0100

    MM-13: Function to display pop-up not working

--- src/components/roverSelector/RoverSelectorPage.tsx
+++ src/components/roverSelector/RoverSelectorPage.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Spirit from "images/SpiritRover.jpg"
+
+function showPopUp(){
+  Document.getElementById("spirit-pop-up").style.display = "flex"
+}
 
 export const RoverSelectorPage: React.FunctionComponent = () => {
+  const [showInfo, setShowInfo] = useState("");
+
   return (
-    <main>
-      hello
-    </main>
+    <div>
+        CuriosityMap
+    </div>
   );
+  
+  // if (showInfo === "") {
+  //   return (
+  //     <>
+  //       <button onClick={() => setShowInfo("spirit")}>Spirit</button>
+  //     </>
+  //   )
+  // };
+  // if (showInfo === "spirit") {
+  //   return (
+  //     <>
+  //       <div id="spirit-pop-up">
+  //         <img src={Spirit} alt="Spirit Rover"/>
+  //         <h3>Spirit Rover</h3>
+  //         <p>Some interesting facts about Spirit Rover</p>
+  //         <button>See Spirit's Route</button>
+  //         <button>Back</button>
+  //       </div>
+  //     </>
+  //   )
+  // };
 };
