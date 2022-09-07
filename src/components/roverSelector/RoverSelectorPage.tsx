2df2177fbb7c0e546430704bc91b1f07fa38aa70
Jack Hughes <Jack.Hughes@softwire.com>
2022-09-07 14:33:53 +0100

    MM-30: Made review changes

--- src/components/roverSelector/RoverSelectorPage.tsx
+++ src/components/roverSelector/RoverSelectorPage.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./RoverSelectorPage.scss";
-import Opportunity from "/images/OpportunityRover.jpg";
-import Spirit from "/images/SpiritRover.jpg";
-import Curiosity from "/images/CuriosityRover.jpg";
+import opportunityImage from "/images/OpportunityRover.jpg";
+import spiritImage from "/images/SpiritRover.jpg";
+import curiosityImage from "/images/CuriosityRover.jpg";
 
 export const RoverSelectorPage: React.FunctionComponent = () => {
   return (
     <div className="rover-selector">
-      <div className="rover curiosity">
-        <img src={Curiosity} alt="Opportunity Rover" />
+      <div className="rover">
+        <img src={curiosityImage} alt="Curiosity Rover" />
         <h3>Curiosity</h3>
       </div>
-      <div className="rover opportunity">
-        <img src={Opportunity} alt="Opportunity Rover" />
+      <div className="rover">
+        <img src={opportunityImage} alt="Opportunity Rover" />
         <h3>Opportunity</h3>
       </div>
-      <div className="rover spirit">
-        <img src={Spirit} alt="Opportunity Rover" />
+      <div className="rover">
+        <img src={spiritImage} alt="Spirit Rover" />
         <h3>Spirit</h3>
       </div>
     </div>
