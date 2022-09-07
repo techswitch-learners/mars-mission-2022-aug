d771e1f4d64360c821d836b9d845f4c9b7469535
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-09-07 14:36:17 +0100

    MM-07: Remove empty spacing box

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -14,7 +14,6 @@ export const Navbar: React.FunctionComponent = () => {
 
   return <nav>
 
-    <div className="emptySpacingBox"></div>
 
     <div id="menuToggle">
       <button onClick = {() => toggleMenu()}>
