94b6c2cdd41cf1ecddd471f5347f887bb35bc6f5
Konstantinos <kon.xen@:gmail.com>
2022-09-07 11:24:33 +0100

    MM-23: laying the bones for the component

--- src/components/quiz/UserDetails.tsx
+++ src/components/quiz/UserDetails.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface User {
+  name: string;
+  displayPic: string;
+}
 
 export const UserDetails: React.FunctionComponent = () => {
+  [userDetails, setUSerDetails] = useState<User>({
+    name: "",
+    displayPic: "",
+  });
+
+  function onSubmit() {
+    setUserDetails({
+      name: "",
+      displayPic: ""
+    });
+  }
+
   return (
     <div>
+      // form for the UserDetails //set the state with the UserDetails
       <h1>This is the UserDetails Component</h1>
     </div>
   );
