d80bbd3825a470cd89afaf5c8b4c44d4169b16b6
tomscambler <tomscambler@gmail.com>
2022-09-07 13:17:07 +0100

    MM-06: Styled buttons and added welcome text

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -6,12 +6,14 @@ export const LandingPage: React.FunctionComponent = () => {
   return (
     <main>
       <img src="/images/image-of-rovers-on-mars.jpg" alt="images of rovers on Mars" className="headline-image"/>
-      <p>Welcome to our informative and entertaining website about MARS!</p>
+      <h2>Welcome, trainee astronaut, to your Mission to Mars!</h2>
+      <p>Here you will learn about the Red Planet and explore its surface.</p>
+      <p>When the training is complete, we will test if you are ready to be an astronaut!</p>
 
-      <div className="landing-page-buttons">
-        <Link to="/rovers"><button>Explore Mars</button></Link>
-        <Link to="/fun-facts"><button>Learn More</button></Link>
-        <Link to="/quiz"><button>Test Your Knowledge</button></Link>
+      <div className="landing-page-button-menu">
+        <Link to="/rovers"><button className="landing-page-button">Explore Mars</button></Link>
+        <Link to="/fun-facts"><button className="landing-page-button">Learn More</button></Link>
+        <Link to="/quiz"><button className="landing-page-button">Test Your Knowledge</button></Link>
       </div>
 
 
