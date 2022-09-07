4e1803f12fd1d9c6bed15956de3e00f36b231943
Leo Ng <Leo.Ng@softwire.com>
2022-09-07 15:12:03 +0100

    MM-16: waiting for change

--- src/components/map/CuriosityMap.tsx
+++ src/components/map/CuriosityMap.tsx
@@ -4,12 +4,12 @@ import "./Map.scss";
 export const CuriosityMap: React.FunctionComponent = () => {
   const [date, setDate] = useState('0');
 
-  function handleClick(day:string) {
-    setDate(day)
-    console.log(date)
+  const handleClick = (day: string) => {
+    setDate(day);
+    console.log(date);
   }
   return (
-    <main>
+    <div className="map-container">
       <div className="map">
         <img src="https://mars.nasa.gov/system/resources/deepzooms/24816_MSL_TraverseMap_Sol2695.jpg" 
         alt="Curiosity" 
@@ -18,6 +18,6 @@ export const CuriosityMap: React.FunctionComponent = () => {
       <div className="button" id="Curiosity-day-1">
         <button onClick={()=>handleClick('1')}>Day 1</button>
       </div>
-    </main>
+    </div>
   );
 };
