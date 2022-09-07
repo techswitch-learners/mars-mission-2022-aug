e547ac0b10285787003a40433064589183f60098
Oli Goodall <Oli.Goodall@softwire.com>
2022-09-07 12:32:11 +0100

    MM-30: Three distinct rover images appropriately placed

--- src/components/roverSelector/RoverSelectorPage.tsx
+++ src/components/roverSelector/RoverSelectorPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./RoverSelectorPage.scss";
-import Opportunity from "/public/images/OpportunityRover.png";
+import Opportunity from "/images/OpportunityRover.jpg";
+import Spirit from "/images/SpiritRover.jpg";
+import Curiosity from "/images/CuriosityRover.jpg";
 
 export const RoverSelectorPage: React.FunctionComponent = () => {
   return (
     <div className="rover-selector">
       <div className="rover curiosity">
-        <img src={Opportunity} alt="Opportunity Rover" />
+        <img src={Curiosity} alt="Opportunity Rover" />
         <h3>Curiosity</h3>
       </div>
       <div className="rover opportunity">
@@ -14,7 +16,7 @@ export const RoverSelectorPage: React.FunctionComponent = () => {
         <h3>Opportunity</h3>
       </div>
       <div className="rover spirit">
-        <img src={Opportunity} alt="Opportunity Rover" />
+        <img src={Spirit} alt="Opportunity Rover" />
         <h3>Spirit</h3>
       </div>
     </div>
