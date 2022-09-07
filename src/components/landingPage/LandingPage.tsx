1539c53dcc83bf43e42ea193b1502583829c0f39
tomscambler <tomscambler@gmail.com>
2022-09-07 11:39:21 +0100

    MM-06: Added buttons that link to the other pages of the site

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./LandingPage.scss";
+import { Link } from "react-router-dom";
 
 export const LandingPage: React.FunctionComponent = () => {
   return (
     <main>
       <img src="/images/image-of-rovers-on-mars.jpg" alt="images of rovers on Mars" className="headline-image"/>
-      <h1>Landing Page</h1>
-      <p>Landing page</p>
+      <p>Welcome to our informative and entertaining website about MARS!</p>
+
+      <div className="landing-page-buttons">
+        <Link to="/rovers"><button>Explore Mars</button></Link>
+        <Link to="/fun-facts"><button>Learn More</button></Link>
+        <Link to="/quiz"><button>Test Your Knowledge</button></Link>
+      </div>
+
+
     </main>
   );
 };
