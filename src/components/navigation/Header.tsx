169b471b738427b1833f0d0ce1ead8f7f96f9574
Leo Ng <Leo.Ng@softwire.com>
2022-09-07 10:47:35 +0100

    MM-42: Fixed comments

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -9,3 +9,4 @@ export const Header: React.FunctionComponent = () => {
     </header>
   );
 };
+ 
--- src/components/photoViewer/PhotoViewer.tsx
+++ src/components/photoViewer/PhotoViewer.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export const PhotoViewer: React.FunctionComponent = () => {
-    return (
-      <div>
-        PhotoViewer
-      </div>
-    );
-  };
-  
+  return (
+    <div>
+      PhotoViewer
+    </div>
+  );
+};
