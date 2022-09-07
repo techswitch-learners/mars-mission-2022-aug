ec82f627c9f4a994f8b2e49fb46636f2ed3bbd28
tomscambler <tomscambler@gmail.com>
2022-09-07 11:30:04 +0100

    MM-06: Added headline image with scss to make it 100% of the width

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "./LandingPage.scss";
 
 export const LandingPage: React.FunctionComponent = () => {
   return (
     <main>
+      <img src="/images/image-of-rovers-on-mars.jpg" alt="images of rovers on Mars" className="headline-image"/>
       <h1>Landing Page</h1>
       <p>Landing page</p>
     </main>
