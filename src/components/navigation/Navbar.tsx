79e6bf8795252a19bfe9014781f7c20a0e3087d9
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-09-07 14:26:46 +0100

    MM-07: Made expanding menu dependent on state

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import { useState } from 'react'
 
 export const Navbar: React.FunctionComponent = () => {
+
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  function toggleMenu(){
+    setIsExpanded(current => !current)
+    console.log(isExpanded)
+  }
+
   return <nav>
 
     <div className="emptySpacingBox"></div>
 
     <div id="menuToggle">
-      <input type="checkbox" />
+      <button onClick = {() => toggleMenu()}>
       
         <span></span>
         <span></span>
         <span></span>
+
+      </button>
       
-        <ul id="menu">
+        <ul className = {`menu ${isExpanded ? "expanded" : "collapsed"}`}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/rovers">Explore Mars</Link></li>
           <li><Link to="/fun-facts">Fun Facts</Link></li>
