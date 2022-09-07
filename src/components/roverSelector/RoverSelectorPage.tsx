0c857d812b2b14c4da86180c89fd8bbc997da31a
Jack Hughes <Jack.Hughes@softwire.com>
2022-09-07 10:58:44 +0100

    MM-30: Created initial layout for rover selector

--- src/components/roverSelector/RoverSelectorPage.tsx
+++ src/components/roverSelector/RoverSelectorPage.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "./RoverSelectorPage.scss";
+import Opportunity from "/public/images/OpportunityRover.png";
 
 export const RoverSelectorPage: React.FunctionComponent = () => {
   return (
-    <main>
-      hello
-    </main>
+    <div className="rover-selector">
+      <div className="rover curiosity">
+        <img src={Opportunity} alt="Opportunity Rover" />
+        <h3>Curiosity</h3>
+      </div>
+      <div className="rover opportunity">
+        <img src={Opportunity} alt="Opportunity Rover" />
+        <h3>Opportunity</h3>
+      </div>
+      <div className="rover spirit">
+        <img src={Opportunity} alt="Opportunity Rover" />
+        <h3>Spirit</h3>
+      </div>
+    </div>
   );
 };
