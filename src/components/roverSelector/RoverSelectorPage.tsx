2b0d9212fd92127e971d08146c005247f1975609
Jack Hughes <Jack.Hughes@softwire.com>
2022-09-07 16:00:43 +0100

    MM-13: Three functioning pop-up windows

--- src/components/roverSelector/RoverSelectorPage.tsx
+++ src/components/roverSelector/RoverSelectorPage.tsx
@@ -5,22 +5,6 @@ import './RoverSelectorPage.scss'
 export const RoverSelectorPage: React.FunctionComponent = () => {
   const [showInfo, setShowInfo] = useState("");
 
-  // function toggleSpiritPopUp() {
-  //   setShowInfo("spirit")
-  // }
-
-  // function toggleOpportunityPopUp() {
-  //   setShowInfo("opportunity")
-  // }
-
-  // function toggleCuriosityPopUp() {
-  //   setShowInfo("curiosity")
-  // }
-
-  // function togglePopUpOff() {
-  //   setShowInfo("")
-  // }
-
   return (
     <div>
         <button onClick={() => setShowInfo("spirit")}>Spirit</button>
@@ -31,27 +15,22 @@ export const RoverSelectorPage: React.FunctionComponent = () => {
           <button>See Spirit's Route</button>
           <button onClick={() => setShowInfo("")}>Back</button>
         </div>
+        <button onClick={() => setShowInfo("opportunity")}>Opportunity</button>
+        <div className= {`opportunity-pop-up ${showInfo === "opportunity" ? "expanded" : "collapsed"}`}>
+          <img src={Spirit} alt="Opportunity Rover"/>
+          <h3>Opportunity Rover</h3>
+          <p>Some interesting facts about Opportunity Rover</p>
+          <button>See Opportunity's Route</button>
+          <button onClick={() => setShowInfo("")}>Back</button>
+        </div>
+        <button onClick={() => setShowInfo("curiosity")}>Curiosity</button>
+        <div className= {`curiosity-pop-up ${showInfo === "curiosity" ? "expanded" : "collapsed"}`}>
+          <img src={Spirit} alt="Curiosity Rover"/>
+          <h3>Curiosity Rover</h3>
+          <p>Some interesting facts about Curiosity Rover</p>
+          <button>See Curiosity's Route</button>
+          <button onClick={() => setShowInfo("")}>Back</button>
+        </div>
     </div>
   );
-  
-  // if (showInfo === "") {
-  //   return (
-  //     <>
-  //       <button onClick={() => setShowInfo("spirit")}>Spirit</button>
-  //     </>
-  //   )
-  // };
-  // if (showInfo === "spirit") {
-  //   return (
-  //     <>
-  //       <div id="spirit-pop-up">
-  //         <img src={Spirit} alt="Spirit Rover"/>
-  //         <h3>Spirit Rover</h3>
-  //         <p>Some interesting facts about Spirit Rover</p>
-  //         <button>See Spirit's Route</button>
-  //         <button>Back</button>
-  //       </div>
-  //     </>
-  //   )
-  // };
 };
