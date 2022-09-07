c12d02bd73584aa61221376a7c8b0a5b8de14e96
Leo Ng <Leo.Ng@softwire.com>
2022-09-07 12:20:43 +0100

    MM-16: added one button and set date staute

--- src/components/map/CuriosityMap.tsx
+++ src/components/map/CuriosityMap.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import './Map.scss'
 
 export const CuriosityMap: React.FunctionComponent = () => {
+  const [date, setDate] = useState('0')
+
+  function handleClick(day:string) {
+    setDate(day)
+    console.log(date)
+  }
   return (
-    <div>
-        <img src="https://mars.nasa.gov/system/resources/deepzooms/24816_MSL_TraverseMap_Sol2695.jpg" alt="Curiosity" width ="300"/>
-    </div>
+    <main>
+
+      <div className="map">
+        <img src="https://mars.nasa.gov/system/resources/deepzooms/24816_MSL_TraverseMap_Sol2695.jpg" 
+        alt="Curiosity" 
+       />
+      </div>
+      <div className="button" id="Curiosity-day-1">
+        <button onClick={()=>handleClick('1')}>Day 1</button>
+      </div>
+      
+      
+    </main>
   );
 };
