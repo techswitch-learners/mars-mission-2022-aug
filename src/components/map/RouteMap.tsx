b8091efcc9d8b00de4235c1f66ce46e2833c816b
Leo Ng <Leo.Ng@softwire.com>
2022-09-08 14:30:10 +0100

    MM-16: resolved comment

--- src/components/map/RouteMap.tsx
+++ src/components/map/RouteMap.tsx
@@ -60,7 +60,7 @@ export const RouteMap: React.FunctionComponent<RouteMapProps> = ({
         onClick={() => {
           setSol(undefined);
         }}>X</button>
-        {photoArr.slice(0,1).map(photo => <img
+        {photoUrls.slice(0,1).map(photo => <img
           src={photo.img_src}
           alt={`Sol ${sol}`}
         />)}
