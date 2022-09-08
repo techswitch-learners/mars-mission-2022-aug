8689b074c4691b702dfb864e146e3b0f3d351cb4
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-09-08 22:00:45 +0100

    MM-26: modified styling

--- src/components/quiz/Reward.tsx
+++ src/components/quiz/Reward.tsx
@@ -6,32 +6,32 @@ interface User {
   displayPicUrl?: string;
 }
 interface RewardProps {
-  score?: number;
+  score: number;
   userDetails: User;
 }
 
 export const Reward: React.FunctionComponent<RewardProps> = ({ score, userDetails }) => {
   const renderMessage = () => {
     if (score < 3) {
-      return <h3>Maybe check the fun facts about mars? Your score is {score}</h3>;
+      return <h3 className="reward-card-print">Maybe check the fun facts about mars? Your score is {score}</h3>;
     }
      if (score > 3 && score <= 6) {
        return (
-         <h3>Not Bad? Your score is {score}</h3>
+         <h3 className="reward-card-print">Not Bad? Your score is {score}</h3>
        );
     }
      if (score > 6 && score <= 8) {
-       return <h3>Very good! Your score is {score}</h3>;
+       return <h3 className="reward-card-print">Very good! Your score is {score}</h3>;
     }
     if (score > 8) {
-      return <h3>Congratulations Martian ! Your score is : {score}</h3>;
+      return <h3 className="reward-card-print">Congratulations Martian ! Your score is : {score}</h3>;
     }
 }
   return (
     <div className="reward-for-user">
-      <img src={userDetails.displayPicUrl} alt="display picture" />
-      <h2>{userDetails.name} </h2>
-     { renderMessage() }
+      <h2 className="user-name">{userDetails.name} </h2>
+      <img className="user-image" src={userDetails.displayPicUrl} alt="display picture" />
+      { renderMessage() }
     </div>
   );
 };
