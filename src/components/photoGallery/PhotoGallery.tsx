6596134ed8c8173356c0ff66b403ccd98aa5dcf6
Disha Pandit <Disha.Pandit@softwire.com>
2022-09-08 14:59:28 +0100

    MM-47: Resolved all the problems as suggested

--- src/components/photoGallery/PhotoGallery.tsx
+++ src/components/photoGallery/PhotoGallery.tsx
@@ -10,22 +10,23 @@ export const PhotoGallery: React.FunctionComponent = () => {
           const url = `https://api.nasa.gov/planetary/apod?count=50&api_key=DEMO_KEY`;
           
           const fetchData = async () => {
-            try {
+            
               const response = await fetch(url);
               const json = await response.json();
-              json.forEach((item: {url: string}) => img_urls.push(item.url));  
-              setImageUrls(img_urls);
-            } catch (error) {
-              console.log("error", error);
-            }
+              //json.forEach((item: {url: string}) => img_urls.push(item.url));  
+              setImageUrls(json.map(item => item.url));
           };
       
           fetchData();
       }, []);
 
   return (
-      <div className = "imageGallery">
-      {imageUrls?.map(url => <img className="spaceImage" src={url} alt = ""/>)}
-          </div>
+      <div className = "image-gallery">
+        {
+          imageUrls === undefined
+          ? <p>Loading....</p>
+          : imageUrls.map(url => <img 
+        className="space-image" src={url} alt = ""/>)}
+      </div>
   );
 };
