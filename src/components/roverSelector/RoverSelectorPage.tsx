bb3cf5573acd6b478f8b706920a34f165c5d8a45
Oli Goodall <Oli.Goodall@softwire.com>
2022-09-08 10:57:11 +0100

    MM-13: Buttons to open pop up are now labelled pictures of rovers

--- src/components/roverSelector/RoverSelectorPage.tsx
+++ src/components/roverSelector/RoverSelectorPage.tsx
@@ -1,31 +1,42 @@
-import React, { useState } from "react";
-import Spirit from "/images/SpiritRover.jpg"
-import './RoverSelectorPage.scss'
+import React, {useState} from "react";
+import "./RoverSelectorPage.scss";
+import opportunityImage from "/images/OpportunityRover.jpg";
+import spiritImage from "/images/SpiritRover.jpg";
+import curiosityImage from "/images/CuriosityRover.jpg";
 
 export const RoverSelectorPage: React.FunctionComponent = () => {
   const [showInfo, setShowInfo] = useState("");
 
   return (
-    <div>
-        <button onClick={() => setShowInfo("spirit")}>Spirit</button>
+    <div className="rover-selector">
+          <div className="rover" onClick={() => setShowInfo("spirit")}>
+          <img src={spiritImage} alt="Spirit Rover" />
+          <h3>Spirit</h3>
+          </div>
         <div className= {`spirit-pop-up ${showInfo === "spirit" ? "expanded" : "collapsed"}`}>
-          <img src={Spirit} alt="Spirit Rover"/>
+          <img src={spiritImage} alt="Spirit Rover"/>
           <h3>Spirit Rover</h3>
           <p>Some interesting facts about Spirit Rover</p>
           <button>See Spirit's Route</button>
           <button onClick={() => setShowInfo("")}>Back</button>
         </div>
-        <button onClick={() => setShowInfo("opportunity")}>Opportunity</button>
+        <div className="rover" onClick={() => setShowInfo("opportunity")}>
+          <img src={opportunityImage} alt="Opportunity Rover" />
+          <h3>Opportunity</h3>
+          </div>
         <div className= {`opportunity-pop-up ${showInfo === "opportunity" ? "expanded" : "collapsed"}`}>
-          <img src={Spirit} alt="Opportunity Rover"/>
+          <img src={opportunityImage} alt="Opportunity Rover"/>
           <h3>Opportunity Rover</h3>
           <p>Some interesting facts about Opportunity Rover</p>
           <button>See Opportunity's Route</button>
           <button onClick={() => setShowInfo("")}>Back</button>
         </div>
-        <button onClick={() => setShowInfo("curiosity")}>Curiosity</button>
+          <div onClick={() => setShowInfo("curiosity")} className="rover">
+            <img src={curiosityImage} alt="Curiosity Rover" />
+           <h3>Curiosity</h3>
+          </div>
         <div className= {`curiosity-pop-up ${showInfo === "curiosity" ? "expanded" : "collapsed"}`}>
-          <img src={Spirit} alt="Curiosity Rover"/>
+          <img src={curiosityImage} alt="Curiosity Rover"/>
           <h3>Curiosity Rover</h3>
           <p>Some interesting facts about Curiosity Rover</p>
           <button>See Curiosity's Route</button>
@@ -34,3 +45,4 @@ export const RoverSelectorPage: React.FunctionComponent = () => {
     </div>
   );
 };
+
