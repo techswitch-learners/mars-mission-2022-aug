677340cf2df5e3cc45e50fd195ea544c4a63d1bf
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-09-08 11:11:45 +0100

    MM-05: Replaced placeholder funfacts with actual fun facts

--- src/components/funFacts/funfacts.ts
+++ src/components/funFacts/funfacts.ts
@@ -7,19 +7,20 @@ export interface FunFacts {
 export const funfacts: FunFacts[] = [
     {
         id: 1,
-        prompt: "funfact1",
-        detail: "detail1",
+        prompt: "Mars is named after the Roman god of war.",
+        detail: "The month march was also named after him. Mars's moons, called Phobos and Deimos, are named after the gods of fear",
     },
 
     {
         id: 2,
-        prompt: "funfact2",
-        detail: "detail2",
+        prompt: "A year on Mars is not the same as on Earth?",
+        detail: "One martian year is about twice as long as a year on Earth, so you only get one birthday every two years!",
     },
 
     {
         id: 3,
-        prompt: "funfact3",
-        detail: "detail3",
+        prompt: "Did you know that gravity is different on Mars? ",
+        detail: "On Mars, you only weigh three eighths of what you weigh on Earth, meaning you could jump more than twice as high!",
     },
+
 ]
