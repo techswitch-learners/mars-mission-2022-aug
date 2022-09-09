3bcd6f46cd0e84e628a9a7df909052fe0af0904e
Disha Pandit <Disha.Pandit@softwire.com>
2022-09-09 08:35:34 +0100

    MM-47: Implemented all the required changes

--- src/components/photoGallery/PhotoGallery.tsx
+++ src/components/photoGallery/PhotoGallery.tsx
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from "react";
-import "./PhotoGallery.scss"
+import "./PhotoGallery.scss";
 
 export const PhotoGallery: React.FunctionComponent = () => {
-
   const [imageUrls, setImageUrls] = useState<string[]>();
-  const img_urls: string[] = [];
 
-        useEffect(() => {
-          const url = `https://api.nasa.gov/planetary/apod?count=50&api_key=DEMO_KEY`;
-          
-          const fetchData = async () => {
-            
-              const response = await fetch(url);
-              const json = await response.json();
-              //json.forEach((item: {url: string}) => img_urls.push(item.url));  
-              setImageUrls(json.map(item => item.url));
-          };
-      
-          fetchData();
-      }, []);
+  useEffect(() => {
+    const url = `https://api.nasa.gov/planetary/apod?count=50&api_key=DEMO_KEY`;
+
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setImageUrls(json.map((item) => item.url));
+    };
+
+    fetchData();
+  }, []);
 
   return (
-      <div className = "image-gallery">
-        {
-          imageUrls === undefined
-          ? <p>Loading....</p>
-          : imageUrls.map(url => <img 
-        className="space-image" src={url} alt = ""/>)}
-      </div>
+    <div className="image-gallery">
+      {imageUrls === undefined ? (
+        <p>Loading....</p>
+      ) : (
+        imageUrls.map((url) => <img className="space-image" src={url} alt="" />)
+      )}
+    </div>
   );
 };
